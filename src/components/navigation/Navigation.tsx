d419052c5d8dfeae54bdace26b24ab774989a92d
Ahmad Haymour <haimour22@gmail.com>
2022-12-24 23:17:29 +0100

    Styling Categories

--- src/components/navigation/Navigation.tsx
+++ src/components/navigation/Navigation.tsx
@@ -1,18 +1,26 @@
 import {Link} from 'react-router-dom'
 import './Navigation.scss'
 
-export default function Navigation() {
+export default function Navigation({children}: any) {
 
   return (
-    <div id='Navigation'>
+    <div className='Navigation'>
+      <div className='nav'>
 
-        <ul className='nav-list'>
-            <li className='item'><Link to={'/'}>Home</Link></li>
-            <li className='item'><Link to={'/drinks'}>Drinks</Link></li>
-            <li className='item'><Link to={'/meals'}>Meals</Link></li>
-            <li className='item'><Link to={'/rated-drinks'}>Rated Drinks</Link></li>   
-            <li className='item'><Link to={'/rated-meals'}>Rated Meals</Link></li>
-        </ul>
+          <ul className='nav-list'>
+              <li className='item'><Link to={'/'}>Home</Link></li>
+              <li className='item'><Link to={'/drinks'}>Drinks</Link></li>
+              <li className='item'><Link to={'/meals'}>Meals</Link></li>
+              <li className='item'><Link to={'/rated-drinks'}>Rated Drinks</Link></li>   
+              <li className='item'><Link to={'/rated-meals'}>Rated Meals</Link></li>
+          </ul>
+      </div>
+      <div className="children">
+        {children}
+      </div>
+      <footer id='Footer'>
+        <h1>Footer</h1>
+      </footer>
     </div>
   )
 }
