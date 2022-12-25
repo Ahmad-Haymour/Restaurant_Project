3c759ae3e57d7ef3356ba4974996d35a318c2883
Ahmad Haymour <haimour22@gmail.com>
2022-12-25 17:47:01 +0100

    Styling Components

--- src/pages/drink/Drink.tsx
+++ src/pages/drink/Drink.tsx
@@ -1,6 +1,7 @@
 import axios from 'axios';
 import React, { useEffect, useState } from 'react'
 import { useParams } from 'react-router-dom';
+import '../meal/Meal.scss'
 
 export type Props = {
   strDrink : string,
@@ -10,31 +11,48 @@ export type Props = {
   strCategory?: string
 }
 
-export default function Drink() {
+export type DetailsProps = {
+  dateModified: string
+  idDrink: string
+  strAlcoholic: string
+  strCategory: string
+  strDrink: string
+  strDrinkThumb: string
+  strGlass: string
+  strIngredient1: string
+  strIngredient2: string
+  strIngredient3:string
+  strIngredient4: string
+  strInstructions: string
+  strInstructionsDE?: string
+}
 
+export default function Drink() {
     let {category} = useParams<string>()
     let {drink_id} = useParams<string>()
+    let [drinkDetails, setDrinkDetails] = useState<DetailsProps | null>(null)
 
-    let [singleDrink, setSingleDrink] = useState<Props | null>(null)
+    useEffect(()=>{
+      const url = "https://www.thecocktaildb.com/api/json/v1/1/lookup.php?i="+drink_id
 
-    useEffect( ()=>{
-        const url = "https://www.thecocktaildb.com/api/json/v1/1/filter.php?c="+category;
-        axios.get(url).then((response)=>{
-          console.log('My Response => ',response);
-          setSingleDrink(response?.data?.drinks?.filter((e:Props)=>e.idDrink === drink_id)[0])
-        })
-    
-      }, [drink_id, category])
-
-      console.log('Single  Drink => ', singleDrink);
-      console.log('Params Drink id => ', drink_id );
+      axios.get(url).then((res)=>{
+        console.log('Drink Details: ', res.data)
+        setDrinkDetails(res.data.drinks[0])
+    })
+    }, [drink_id])
 
   return (
-    <div id='Drinks'>
-        <h1>Drink Category</h1>
-        <h2>{category}</h2>
-        <h1>{singleDrink?.strDrink}</h1>
-        <img src={singleDrink?.strDrinkThumb} alt="Drink Thumb" /> 
+    <div id='Drink'>
+        <img src={drinkDetails?.strDrinkThumb} alt="Meal Thumb"/> 
+        <ul>
+          <li><span className="white"> Area:&nbsp; </span> {category}</li>
+          <li><span className="white">Drink:&nbsp; </span>{drinkDetails?.strAlcoholic}</li>
+          <li><span className="white">Category:  </span>{drinkDetails?.strCategory}</li>
+          <li><span className="white">Glass:&nbsp;  </span>{drinkDetails?.strGlass}</li>
+          {drinkDetails &&<li><span className="white">Ingredients:&nbsp; </span>{drinkDetails.strIngredient1}, {drinkDetails.strIngredient2}, {drinkDetails.strIngredient3}, {drinkDetails.strIngredient4}</li>}
+          <li> <span className="white"> Last Modified: &nbsp; </span>{drinkDetails?.dateModified ? drinkDetails?.dateModified : '2022-12-23 22:49:04'}</li>
+          <li><span className="white">Description:&nbsp; </span>{drinkDetails?.strInstructions}</li>
+        </ul>
     </div>
   )
 }
--- src/pages/meal/Meal.tsx
+++ src/pages/meal/Meal.tsx
@@ -1,7 +1,7 @@
 import axios from 'axios';
 import React, { useEffect, useState } from 'react'
 import { useParams } from 'react-router-dom';
-
+import './Meal.scss'
 
 export type Props = {
   strMeal : string,
@@ -11,12 +11,26 @@ export type Props = {
   strCategory?: string
 }
 
-export default function Meal() {
+export type DetailsProps = {
+  dateModified: string
+  idMeal: string
+  strCategory: string
+  strMeal: string
+  strMealThumb: string
+  strTags: string
+  strIngredient1: string
+  strIngredient2: string
+  strIngredient3:string
+  strIngredient4: string
+  strInstructions: string
+  strInstructionsDE?: string
+}
 
+export default function Meal() {
     let {land} = useParams<string>()
     let {meal_id} = useParams<string>()
-
     let [singleMeal, setSingleMeal] = useState<Props | null>(null)
+    let [mealDetails, setMealDetails] = useState<DetailsProps | null>(null)
 
     useEffect(() => {
         const url = "https://www.themealdb.com/api/json/v1/1/filter.php?a="+land;
@@ -26,12 +40,26 @@ export default function Meal() {
         })
     }, [land, meal_id])
 
+    useEffect(()=>{
+      const url = "https://www.themealdb.com/api/json/v1/1/lookup.php?i="+meal_id
+      axios.get(url).then((res)=>{
+        console.log('Meal Details: ', res.data)
+        setMealDetails(res.data.meals[0])
+    })
+    }, [meal_id])
+
   return (
     <div id='Meal'>
-        <h1>Meal Category</h1>
-        <h2>{land}</h2>
-        <h1>{singleMeal?.strMeal}</h1>
-        <img src={singleMeal?.strMealThumb} alt="Meal Thumb" /> 
+        <img src={singleMeal?.strMealThumb} alt="Meal Thumb"/>
+        <ul>
+          <li><span className="white"> Area:&nbsp; </span> {land}</li>
+          <li><span className="white">Meal:&nbsp; </span>{mealDetails?.strMeal}</li>
+          <li><span className="white">Category:  </span>{mealDetails?.strCategory}</li>
+          <li><span className="white">Tags:&nbsp;  </span>{mealDetails?.strTags}</li>
+          {mealDetails &&<li><span className="white">Ingredients:&nbsp; </span>{mealDetails.strIngredient1}, {mealDetails.strIngredient2}, {mealDetails.strIngredient3}, {mealDetails.strIngredient4}</li>}
+          <li> <span className="white"> Date: &nbsp; </span>{mealDetails?.dateModified ? mealDetails?.dateModified : '2022-12-25 22:49:04'}</li>
+          <li><span className="white">Description:&nbsp; </span>{mealDetails?.strInstructions}</li>
+        </ul>
     </div>
   )
 }
